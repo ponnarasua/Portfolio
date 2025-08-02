7658e7641459c0647ae02b6e231deb90cc3ef3dc
Ponnarasu <ponnarasua22it@srishakthi.ac.in>
2025-08-02 10:21:38 +0530

    Adjust HeroSection positioning: modify bottom offset for improved layout

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -122,7 +122,7 @@ const HeroSection: React.FC = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 2, duration: 1 }}
-          className="absolute -bottom-40 left-1/2 transform -translate-x-1/2"
+          className="absolute -bottom-20 left-1/2 transform -translate-x-1/2"
         >
           <motion.div
             animate={{ y: [0, 10, 0] }}
